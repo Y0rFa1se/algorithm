1fa4a16cdde916daa1074a0dc046d19da6ab6908
Y0rFa1se <neargul123@gmail.com>
2023-02-01 20:50:02 +0900

    d

---
+++ 무식하게 풀기(완전 탐색)/알고스팟/시계 맞추기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int c;
+	cin >> c;
+
+	for (int i = 0; i < c; ++i){
+		int clocks[16];
+	}
+}
---
+++ 분할 정복/알고스팟/울타리 잘라내기.py
@@ -0,0 +1,91 @@
+from sys import stdin; from sys import stdout
+
+def put(): return stdin.readline().replace("\n", "")
+def putint(): return int(stdin.readline().replace("\n", ""))
+def putfloat(): return float(stdin.readline().replace("\n", ""))
+def putfor(times: int):
+	ret = []
+	for i in range(times):
+		ret.append(stdin.readline().replace("\n", ""))
+	return ret
+def putintfor(times: int):
+	ret = []
+	for i in range(times):
+		ret.append(int(stdin.readline().replace("\n", "")))
+	return ret
+def putfloatfor(times: int):
+	ret = []
+	for i in range(times):
+		ret.append(float(stdin.readline().replace("\n", "")))
+	return ret
+def mapint(arg: str = " "): return map(int, stdin.readline().split(arg))
+def mapfloat(arg: str = " "): return map(float, stdin.readline().split(arg))
+def mapintfor(times: int, arg: str = " "):
+	ret = []
+	for i in range(times):
+		_ = stdin.readline().replace("\n", "")
+		if len(_) == 1:
+			ret.append(int(_))
+		else:
+			ret.append(map(int, _.split(arg)))
+	return ret
+def mapfloatfor(times: int, arg: str = " "):
+	ret = []
+	for i in range(times):
+		_ = stdin.readline().replace("\n", "")
+		if len(_) == 1:
+			ret.append(int(_))
+		else:
+			ret.append(map(float, _.split(arg)))
+	return ret
+def out(arg): stdout.writelines(str(arg))
+def newl(): stdout.writelines("\n")
+
+
+def exhaustive_search(fences):
+	def Contiguous(fences, loc, nof, hei):
+		cnt = 0
+		ind = loc
+		while (True):
+			if (ind == -1):
+				break
+
+			if (fences[ind] >= hei):
+				cnt += 1
+				ind -= 1
+
+			else:
+				break
+
+		ind = loc + 1
+		while (True):
+			if (ind == nof):
+				break
+
+			if (fences[ind] >= hei):
+				cnt += 1
+				ind += 1
+
+			else:
+				break
+			
+		return hei * cnt
+	nof = len(fences)
+	for j in range(nof):
+		hei = fences[j]
+		if j != 0:
+			if (last != hei):
+				max_square = max(Contiguous(fences, j, nof, hei), max_square)
+
+		else:
+			last = 0
+			max_square = Contiguous(fences, j, nof, hei)
+	return max_square
+
+def divide_and_conquer(fences):
+	
+
+for i in range(putint()):
+	put()
+	fences = list(mapint())
+	out(exhaustive_search(fences)); newl()
