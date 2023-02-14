e0b13bdc99d88b94205a93f2efe9a745b2dcb9be
Y0rFa1se <neargul123@gmail.com>
2023-02-15 00:23:50 +0900

    d

---
+++ 동적 계획법/알고스팟/와일드 카드.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	int c;
+	cin >> c;
+
+	string wild;
+	int n;
+	string _;
+	vector<string> ans;
+	for (int i = 0; i < c; i++){
+		cin >> wild;
+		cin >> n;
+		for (int j = 0; j < n; j++){
+			cin >> _;
+		}
+	}
+}
